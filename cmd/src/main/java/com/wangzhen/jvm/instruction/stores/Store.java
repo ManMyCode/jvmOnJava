cd7da9866e1acb4a5bbbbffa7d464e80ecbf5c0a
wangzhen <1228857713@qq.com>
2020-06-22 13:20:32 +0800

    完成stack 指令

---
+++ cmd/src/main/java/com/wangzhen/jvm/instruction/stores/Store.java
@@ -0,0 +1,44 @@
+package com.wangzhen.jvm.instruction.stores;
+
+import com.wangzhen.jvm.runtimeData.JFrame;
+import com.wangzhen.jvm.runtimeData.helap.JObject;
+
+public class Store {
+    public static void astore(JFrame frame, int index) {
+        JObject ref = frame.getOperandStack().popRef();
+        frame.getLocalVars().setRef(index, ref);
+    }
+
+    public static void istore(JFrame frame, int index) {
+        int val = frame.getOperandStack().popInt();
+        frame.getLocalVars().setInt(index, val);
+    }
+
+    public static void lstore(JFrame frame, int index) {
+        long val = frame.getOperandStack().popLong();
+        frame.getLocalVars().setLong(index, val);
+    }
+
+    public static void fstore(JFrame frame, int index) {
+        float val = frame.getOperandStack().popFLoat();
+        frame.getLocalVars().setFloat(index, val);
+    }
+
+    public static void dstote(JFrame frame, int index) {
+        double val = frame.getOperandStack().popDouble();
+        frame.getLocalVars().setDouble(index, val);
+    }
+
+    //用在 store 数组元素时，检测数组是否为 null
+    public static void checkNotNull(JObject arrRef) {
+        if (arrRef == null) {
+            throw new NullPointerException();
+        }
+    }
+
+    public static void checkIndex(int count, int index) {
+        if (index < 0 || index >= count) {
+            throw new ArrayIndexOutOfBoundsException("index: " + index + " array's count: " + count);
+        }
+    }
+}
---
+++ cmd/src/main/java/com/wangzhen/jvm/instruction/stores/storeref/ASTORE_2.java
@@ -0,0 +1,18 @@
+package com.wangzhen.jvm.instruction.stores.storeref;
+
+import com.wangzhen.jvm.instruction.base.Index8Instruction;
+import com.wangzhen.jvm.instruction.base.NoOperandsInstruction;
+import com.wangzhen.jvm.instruction.stores.Store;
+import com.wangzhen.jvm.runtimeData.JFrame;
+
+/**
+ * Author: zhangxin
+ * Time: 2017/5/5 0005.
+ * Desc:
+ */
+public class ASTORE_2 extends NoOperandsInstruction {
+    @Override
+    public void execute(JFrame frame) {
+        Store.astore(frame,2);
+    }
+}
