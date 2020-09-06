8cacd3b81287045bd3dd737cd6a2b367fecb11fa
wangzhen <1228857713@qq.com>
2020-09-06 14:37:46 +0800

    提交2个 leetcode 题目

---
+++ Concurrent/src/main/java/com/wangzhen/concurrent/juc/semaphore/testSemaphore.java
@@ -0,0 +1,36 @@
+package com.wangzhen.concurrent.juc.semaphore;
+
+import com.wangzhen.concurrent.util.SleepUtils;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Semaphore;
+
+public class testSemaphore {
+    static Logger logger = LoggerFactory.getLogger(testSemaphore.class);
+
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(3);
+
+
+        for (int i = 0; i <5 ; i++) {
+            new Thread(()->{
+                try {
+                    // 获得许可
+                    semaphore.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    logger.debug("running....");
+                    SleepUtils.second(1);
+                    logger.debug("ending....");
+                }finally {
+                    semaphore.release();
+                }
+            }).start();
+        }
+    }
+}
---
+++ Concurrent/src/main/java/com/wangzhen/concurrent/pattern/同步模式之顺序控制/ParkAndUnpark.java
@@ -0,0 +1,10 @@
+package com.wangzhen.concurrent.pattern.同步模式之顺序控制;
+
+import org.junit.jupiter.api.Test;
+
+public class ParkAndUnpark {
+    @Test
+    public void test(){
+
+    }
+}
