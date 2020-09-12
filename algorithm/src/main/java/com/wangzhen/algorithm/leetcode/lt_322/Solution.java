2cbaa03f9205a0a5d14af1210d28aec8334cd459
wangzhen <1228857713@qq.com>
2020-09-12 17:34:35 +0800

    添加书相关的代码

---
+++ algorithm/src/main/java/com/wangzhen/algorithm/leetcode/lt_322/Solution.java
@@ -0,0 +1,20 @@
+package com.wangzhen.algorithm.leetcode.lt_322;
+
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TreeVisitor;
+
+import java.util.TreeMap;
+
+/**
+ * Description: 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * Datetime:    2020/9/10   10:53 下午
+ * Author:   王震
+ */
+class Solution {
+    public int coinChange(int[] coins, int sum) {
+        TreeMap map = new TreeMap();
+        return 0;
+    }
+
+
+}
---
+++ algorithm/src/main/java/com/wangzhen/algorithm/leetcode/lt_53/Solution.java
@@ -0,0 +1,25 @@
+package com.wangzhen.algorithm.leetcode.lt_53;
+
+/**
+ * Description: leetcode 第53题 最大子序和
+ * Datetime:    2020/9/9   9:20 下午
+ * Author:   王震
+ */
+public class Solution {
+
+    public int maxSubArray(int[] nums) {
+        int ans=Integer.MIN_VALUE;
+        int sum =0;
+        for (int num:nums){
+            if(sum>0){
+                sum +=num;
+            }else{
+                sum = num;
+            }
+            ans = Math.max(sum,ans);
+        }
+        return ans;
+    }
+
+
+}
