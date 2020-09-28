89d9904a3c49757881764eec4fdf6517ca825cdb
wangzhen <1228857713@qq.com>
2020-09-28 18:12:58 +0800

    完善指令集

--- jvmx/src/main/java/com/wangzhen/jvm/instructions/InstructionFactory.java
+++ jvmx/src/main/java/com/wangzhen/jvm/instructions/InstructionFactory.java
@@ -1,6 +1,7 @@
 package com.wangzhen.jvm.instructions;
 
 
+
 import com.wangzhen.jvm.instructions.base.Instruction;
 import com.wangzhen.jvm.instructions.base.NoOperandsInstruction;
 import com.wangzhen.jvm.instructions.comparisons.dcmp.DCMPG;
@@ -19,6 +20,18 @@ import com.wangzhen.jvm.instructions.constants.SIPUSH;
 import com.wangzhen.jvm.instructions.constants.*;
 import com.wangzhen.jvm.instructions.control.GOTO;
 import com.wangzhen.jvm.instructions.control.RETURN;
+import com.wangzhen.jvm.instructions.conversions.d2x.D2F;
+import com.wangzhen.jvm.instructions.conversions.d2x.D2I;
+import com.wangzhen.jvm.instructions.conversions.d2x.D2L;
+import com.wangzhen.jvm.instructions.conversions.f2x.F2D;
+import com.wangzhen.jvm.instructions.conversions.f2x.F2I;
+import com.wangzhen.jvm.instructions.conversions.f2x.F2L;
+import com.wangzhen.jvm.instructions.conversions.i2x.I2D;
+import com.wangzhen.jvm.instructions.conversions.i2x.I2F;
+import com.wangzhen.jvm.instructions.conversions.i2x.I2L;
+import com.wangzhen.jvm.instructions.conversions.l2x.L2D;
+import com.wangzhen.jvm.instructions.conversions.l2x.L2F;
+import com.wangzhen.jvm.instructions.conversions.l2x.L2I;
 import com.wangzhen.jvm.instructions.loads.loadDouble.*;
 import com.wangzhen.jvm.instructions.loads.loadFloat.*;
 import com.wangzhen.jvm.instructions.loads.loadInt.ILOAD;
@@ -26,15 +39,48 @@ import com.wangzhen.jvm.instructions.loads.loadInt.*;
 import com.wangzhen.jvm.instructions.loads.loadlong.*;
 import com.wangzhen.jvm.instructions.loads.loadref.*;
 import com.wangzhen.jvm.instructions.loads.loadxarr.*;
+import com.wangzhen.jvm.instructions.math.add.DADD;
+import com.wangzhen.jvm.instructions.math.add.FADD;
 import com.wangzhen.jvm.instructions.math.add.IADD;
+import com.wangzhen.jvm.instructions.math.add.LADD;
+import com.wangzhen.jvm.instructions.math.and.IAND;
+import com.wangzhen.jvm.instructions.math.and.LAND;
+import com.wangzhen.jvm.instructions.math.div.DDIV;
+import com.wangzhen.jvm.instructions.math.div.FDIV;
+import com.wangzhen.jvm.instructions.math.div.IDIV;
+import com.wangzhen.jvm.instructions.math.div.LDIV;
+import com.wangzhen.jvm.instructions.math.iinc.IINC;
+import com.wangzhen.jvm.instructions.math.mul.DMUL;
+import com.wangzhen.jvm.instructions.math.mul.FMUL;
+import com.wangzhen.jvm.instructions.math.mul.IMUL;
+import com.wangzhen.jvm.instructions.math.mul.LMUL;
+import com.wangzhen.jvm.instructions.math.neg.DNEG;
+import com.wangzhen.jvm.instructions.math.neg.FNEG;
+import com.wangzhen.jvm.instructions.math.neg.INEG;
+import com.wangzhen.jvm.instructions.math.neg.LNEG;
+import com.wangzhen.jvm.instructions.math.or.IOR;
+import com.wangzhen.jvm.instructions.math.or.LOR;
+import com.wangzhen.jvm.instructions.math.rem.DREM;
+import com.wangzhen.jvm.instructions.math.rem.FREM;
+import com.wangzhen.jvm.instructions.math.rem.IREM;
+import com.wangzhen.jvm.instructions.math.rem.LREM;
+import com.wangzhen.jvm.instructions.math.sub.DSUB;
+import com.wangzhen.jvm.instructions.math.sub.FSUB;
+import com.wangzhen.jvm.instructions.math.sub.ISUB;
+import com.wangzhen.jvm.instructions.math.sub.LSUB;
+import com.wangzhen.jvm.instructions.math.xor.IXOR;
+import com.wangzhen.jvm.instructions.math.xor.LXOR;
 import com.wangzhen.jvm.instructions.references.*;
-import com.wangzhen.jvm.instructions.stack.dup.DUP;
+import com.wangzhen.jvm.instructions.stack.dup.*;
+import com.wangzhen.jvm.instructions.stack.pop.POP;
+import com.wangzhen.jvm.instructions.stack.pop.POP2;
+import com.wangzhen.jvm.instructions.stack.swap.SWAP;
 import com.wangzhen.jvm.instructions.stores.storedouble.*;
 import com.wangzhen.jvm.instructions.stores.storefloat.*;
 import com.wangzhen.jvm.instructions.stores.storeint.*;
 import com.wangzhen.jvm.instructions.stores.storelong.*;
 import com.wangzhen.jvm.instructions.stores.storeref.*;
-import com.wangzhen.jvm.instructions.stores.storexarr.IASTORE;
+import com.wangzhen.jvm.instructions.stores.storexarr.*;
 
 
 /**
@@ -145,6 +191,68 @@ public class InstructionFactory {
     static FSTORE fstore = new FSTORE();
     static DSTORE dstore = new DSTORE();
     static ASTORE astore = new ASTORE();
+
+    static LASTORE lastore = new LASTORE();
+    static FASTORE fastore = new FASTORE();
+    static DASTORE dastore = new DASTORE();
+    static AASTORE aastore = new AASTORE();
+    static BASTORE bastore = new BASTORE();
+    static CASTORE castore = new CASTORE();
+    static SASTORE sastore = new SASTORE();
+    static POP pop = new POP();
+    static POP2 pop2 =new POP2();
+    static DUP_X1 dup_x1 = new DUP_X1();
+    static DUP_X2 dup_x2 = new DUP_X2();
+    static DUP2 dup2 = new DUP2();
+    static DUP2_X1 dup2_x1 = new DUP2_X1();
+    static DUP2_X2 dup2_x2 = new DUP2_X2();
+    static SWAP swap = new SWAP();
+    static LADD ladd = new LADD();
+    static FADD fadd = new FADD();
+    static DADD dadd = new DADD();
+    static ISUB isub = new ISUB();
+    static LSUB lsub = new LSUB();
+    static FSUB fsub = new FSUB();
+    static DSUB dsub = new DSUB();
+    static IMUL imul = new IMUL();
+    static LMUL lmul = new LMUL();
+    static FMUL fmul = new FMUL();
+    static DMUL dmul = new DMUL();
+    static IDIV idiv = new IDIV();
+    static LDIV ldiv = new LDIV();
+    static FDIV fdiv = new FDIV();
+    static DDIV ddiv = new DDIV();
+    static IREM irem = new IREM();
+    static LREM lrem = new LREM();
+    static FREM frem = new FREM();
+    static DREM drem = new DREM();
+    static INEG ineg = new INEG();
+    static LNEG lneg = new LNEG();
+    static FNEG fneg = new FNEG();
+    static DNEG dneg = new DNEG();
+    static IAND iand = new IAND();
+    static LAND land = new LAND();
+    static IOR ior = new IOR();
+    static LOR lor = new LOR();
+    static IXOR ixor = new IXOR();
+    static LXOR lxor = new LXOR();
+    static IINC iinc = new IINC();
+    static I2L i2l = new I2L();
+    static I2F i2f = new I2F();
+    static I2D i2d = new I2D();
+    static L2I l2i = new L2I();
+    static L2F l2f = new L2F();
+    static L2D l2d = new L2D();
+    static F2I f2i = new F2I();
+    static F2L f2l = new F2L();
+    static F2D f2d = new F2D();
+    static D2I d2i = new D2I();
+    static D2L d2l = new D2L();
+    static D2F d2f = new D2F();
+//    static I2B i2b = new I2B();
+//    static I2C i2c = new I2C();
+//    static I2S i2s = new I2S();
+
 
 
 
@@ -311,86 +419,86 @@ public class InstructionFactory {
                 return astore_3;
             case 0x4f:
                  return iastore;
-//            case 0x50:
-//                return lastore;
-//            case 0x51:
-//                return fastore;
-//            case 0x52:
-//                return dastore;
-//            case 0x53:
-//                return aastore;
-//            case 0x54:
-//                return bastore;
-//            case 0x55:
-//                return castore;
-//            case 0x56:
-//                return sastore;
-//            case 0x57:
-//                return pop;
-//            case 0x58:
-//                return pop2;
+            case 0x50:
+                return lastore;
+            case 0x51:
+                return fastore;
+            case 0x52:
+                return dastore;
+            case 0x53:
+                return aastore;
+            case 0x54:
+                return bastore;
+            case 0x55:
+                return castore;
+            case 0x56:
+                return sastore;
+            case 0x57:
+                return pop;
+            case 0x58:
+                return pop2;
             case 0x59:
                 return dup;
-//            case 0x5a:
-//                return dup_x1;
-//            case 0x5b:
-//                return dup_x2;
-//            case 0x5c:
-//                return dup2;
-//            case 0x5d:
-//                return dup2_x1;
-//            case 0x5e:
-//                return dup2_x2;
-//            case 0x5f:
-//                return swap;
+            case 0x5a:
+                return dup_x1;
+            case 0x5b:
+                return dup_x2;
+            case 0x5c:
+                return dup2;
+            case 0x5d:
+                return dup2_x1;
+            case 0x5e:
+                return dup2_x2;
+            case 0x5f:
+                return swap;
             case 0x60:
                 return iadd;
-//            case 0x61:
-//                return ladd;
-//            case 0x62:
-//                return fadd;
-//            case 0x63:
-//                return dadd;
-//            case 0x64:
-//                return isub;
-//            case 0x65:
-//                return lsub;
-//            case 0x66:
-//                return fsub;
-//            case 0x67:
-//                return dsub;
-//            case 0x68:
-//                return imul;
-//            case 0x69:
-//                return lmul;
-//            case 0x6a:
-//                return fmul;
-//            case 0x6b:
-//                return dmul;
-//            case 0x6c:
-//                return idiv;
-//            case 0x6d:
-//                return ldiv;
-//            case 0x6e:
-//                return fdiv;
-//            case 0x6f:
-//                return ddiv;
-//            case 0x70:
-//                return irem;
-//            case 0x71:
-//                return lrem;
-//            case 0x72:
-//                return frem;
-//            case 0x73:
-//                return drem;
-//            case 0x74:
-//                return ineg;
-//            case 0x75:
-//                return lneg;
-//            case 0x76:
-//                return fneg;
-//            case 0x77:
-//                return dneg;
+            case 0x61:
+                return ladd;
+            case 0x62:
+                return fadd;
+            case 0x63:
+                return dadd;
+            case 0x64:
+                return isub;
+            case 0x65:
+                return lsub;
+            case 0x66:
+                return fsub;
+            case 0x67:
+                return dsub;
+            case 0x68:
+                return imul;
+            case 0x69:
+                return lmul;
+            case 0x6a:
+                return fmul;
+            case 0x6b:
+                return dmul;
+            case 0x6c:
+                return idiv;
+            case 0x6d:
+                return ldiv;
+            case 0x6e:
+                return fdiv;
+            case 0x6f:
+                return ddiv;
+            case 0x70:
+                return irem;
+            case 0x71:
+                return lrem;
+            case 0x72:
+                return frem;
+            case 0x73:
+                return drem;
+            case 0x74:
+                return ineg;
+            case 0x75:
+                return lneg;
+            case 0x76:
+                return fneg;
+            case 0x77:
+                return dneg;
 //            case 0x78:
 //                return ishl;
 //            case 0x79:
@@ -403,44 +511,44 @@ public class InstructionFactory {
 //                return iushr;
 //            case 0x7d:
 //                return lushr;
-//            case 0x7e:
-//                return iand;
-//            case 0x7f:
-//                return land;
-//            case 0x80:
-//                return ior;
-//            case 0x81:
-//                return lor;
-//            case 0x82:
-//                return ixor;
-//            case 0x83:
-//                return lxor;
-//            case 0x84:
-//                return new IINC();
-//            case 0x85:
-//                return i2l;
-//            case 0x86:
-//                return i2f;
-//            case 0x87:
-//                return i2d;
-//            case 0x88:
-//                return l2i;
-//            case 0x89:
-//                return l2f;
-//            case 0x8a:
-//                return l2d;
-//            case 0x8b:
-//                return f2i;
-//            case 0x8c:
-//                return f2l;
-//            case 0x8d:
-//                return f2d;
-//            case 0x8e:
-//                return d2i;
-//            case 0x8f:
-//                return d2l;
-//            case 0x90:
-//                return d2f;
+            case 0x7e:
+                return iand;
+            case 0x7f:
+                return land;
+            case 0x80:
+                return ior;
+            case 0x81:
+                return lor;
+            case 0x82:
+                return ixor;
+            case 0x83:
+                return lxor;
+            case 0x84:
+                return new IINC();
+            case 0x85:
+                return i2l;
+            case 0x86:
+                return i2f;
+            case 0x87:
+                return i2d;
+            case 0x88:
+                return l2i;
+            case 0x89:
+                return l2f;
+            case 0x8a:
+                return l2d;
+            case 0x8b:
+                return f2i;
+            case 0x8c:
+                return f2l;
+            case 0x8d:
+                return f2d;
+            case 0x8e:
+                return d2i;
+            case 0x8f:
+                return d2l;
+            case 0x90:
+                return d2f;
 //            case 0x91:
 //                return i2b;
 //            case 0x92:
