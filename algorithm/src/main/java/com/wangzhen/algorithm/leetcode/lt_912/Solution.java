e726c28c3375858e665d256173b462bbc8fd4641
wangzhen <1228857713@qq.com>
2020-09-29 14:48:46 +0800

    修改错误的地方

--- algorithm/src/main/java/com/wangzhen/algorithm/leetcode/lt_912/Solution.java
+++ algorithm/src/main/java/com/wangzhen/algorithm/leetcode/lt_912/Solution.java
@@ -11,7 +11,7 @@ import java.util.Random;
 class Solution {
     public static void main(String[] args) {
         //System.out.println(new Random().nextInt(4));
-        int []array = {9,5,2,4,6,1,-1,6,0,33,43,22};
+        int []array = {-74,48,-20,2,10,-84,-5,-9,11,-24,-91,2,-71,64,63,80,28,-30,-58,-11,-44,-87,-22,54,-74,-10,-55,-28,-46,29,10,50,-72,34,26,25,8,51,13,30,35,-8,50,65,-6,16,-2,21,-78,35,-13,14,23,-3,26,-90,86,25,-56,91,-13,92,-25,37,57,-20,-69,98,95,45,47,29,86,-28,73,-44,-46,65,-84,-96,-24,-12,72,-68,93,57,92,52,-45,-2,85,-63,56,55,12,-85,77,-39};
         Solution solution = new Solution();
         solution.sortArray(array);
         for (int i = 0; i < array.length; i++) {
@@ -25,7 +25,7 @@ class Solution {
     public void quickSort(int []arr,int left,int right){
         if (left<right){
             int part = partition(arr,left,right);
-            quickSort(arr,0,part-1);
+            quickSort(arr,left,part-1);
             quickSort(arr,part+1,right);
         }
     }
