24d7ec3a69383eb746b2ee3d18b2b76adfad2e9d
王震 <1228857713@qq.com>
2020-09-30 10:52:24 +0800

    leetcode 701 实现

--- algorithm/src/main/java/com/wangzhen/algorithm/leetcode/tree/lt_701/Solution.java
+++ algorithm/src/main/java/com/wangzhen/algorithm/leetcode/tree/lt_701/Solution.java
@@ -9,6 +9,25 @@ import com.wangzhen.algorithm.leetcode.tree.common.TreeNode;
  */
 class Solution {
     public TreeNode insertIntoBST(TreeNode root, int val) {
-        return null;
+        TreeNode node  = new TreeNode(val);
+        if(root == null)
+            return node;
+
+        if(val<root.val){
+            if(root.left!=null){
+                insertIntoBST(root.left,val);
+            }else {
+                root.left = node;
+            }
+        }else {
+            if(root.right!=null){
+                insertIntoBST(root.right,val);
+            }else {
+                root.right = node;
+            }
+        };
+        return root;
     }
+
+
 }
