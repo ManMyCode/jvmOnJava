20cd7ac7979ae07167ab8899f8579cc26593804d
wangzhen <1228857713@qq.com>
2020-09-30 11:01:51 +0800

    提交题目

---
+++ algorithm/src/main/java/com/wangzhen/algorithm/leetcode/tree/lt_543/Solution.java
@@ -0,0 +1,14 @@
+package com.wangzhen.algorithm.leetcode.tree.lt_543;
+
+import com.wangzhen.algorithm.leetcode.tree.common.TreeNode;
+
+/**
+ * Description: 543. 二叉树的直径
+ * Datetime:    2020/9/30   11:00 上午
+ * Author:   王震
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        return -1;
+    }
+}
