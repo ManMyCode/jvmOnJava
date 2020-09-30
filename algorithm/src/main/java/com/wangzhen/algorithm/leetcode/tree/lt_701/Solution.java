5dbfd3787f20d5ebcd5773cfe0e7e6549dd87856
wangzhen <1228857713@qq.com>
2020-09-30 10:46:25 +0800

    提交题目

---
+++ algorithm/src/main/java/com/wangzhen/algorithm/leetcode/tree/lt_701/Solution.java
@@ -0,0 +1,14 @@
+package com.wangzhen.algorithm.leetcode.tree.lt_701;
+
+import com.wangzhen.algorithm.leetcode.tree.common.TreeNode;
+
+/**
+ * Description: 701. 二叉搜索树中的插入操作
+ * Datetime:    2020/9/30   10:45 上午
+ * Author:   王震
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        return null;
+    }
+}
