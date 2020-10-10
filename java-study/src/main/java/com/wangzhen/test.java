0bea62ed681b680414a46b63eec3265252670567
wangzhen <1228857713@qq.com>
2020-10-10 09:43:31 +0800

    add java-11 分支

---
+++ java-study/src/main/java/com/wangzhen/test.java
@@ -0,0 +1,17 @@
+package com.wangzhen;
+
+import java.util.HashMap;
+
+/**
+ * Description:
+ * Datetime:    2020/10/9   11:25 下午
+ * Author:   王震
+ */
+public class test {
+    public static void main(String[] args) {
+        HashMap<Object, Object> map = new HashMap<>();
+        map.put(1,1);
+        map.get(1);
+
+    }
+}
