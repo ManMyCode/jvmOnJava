a7976915b1392d3488a8d7d623369425e021ec40
wangzhen <1228857713@qq.com>
2020-10-19 09:32:23 +0800

    提交引用等相关代码

--- java-study/src/main/java/com/wangzhen/ref/threadlocal/TestThreadLocal.java
+++ java-study/src/main/java/com/wangzhen/ref/threadlocal/TestThreadLocal.java
@@ -1,6 +1,7 @@
 package com.wangzhen.ref.threadlocal;
 
 
+import org.junit.Test;
 
 public class TestThreadLocal {
     static ThreadLocal<Person> threadLocal = new ThreadLocal<>();
@@ -20,13 +21,35 @@ public class TestThreadLocal {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            threadLocal.set(new Person());
+            threadLocal.set(new Person("p1"));
         }).start();
 
     }
 
+    /**
+     同一个线程去操作的时候会覆盖之前设置的值 因为他是以 threadlocal 为key
+     */
+    @Test
+    public void test02(){
+        ThreadLocal<Person> person = new ThreadLocal<>();
+        person.set(new Person("p1"));
+        person.set(new Person("p2"));
+        System.out.println(person.get().toString());
+    }
+
     static class Person{
-        String name ="zhangshan";
+        String name;
+
+        public Person(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
     }
 }
 
