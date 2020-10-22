80e843bb83f3f85f95246ab27f397a273cc70f82
王震 <1228857713@qq.com>
2020-10-22 16:05:21 +0800

    添加一下 测试代码

---
+++ java-study/src/main/java/com/wangzhen/other/TestObject.java
@@ -0,0 +1,30 @@
+package com.wangzhen.other;
+
+/**
+ * Description:
+ * Datetime:    2020/10/22   10:46
+ * Author:   王震
+ */
+public class TestObject {
+    String name ;
+    String age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TestObject that = (TestObject) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        return age != null ? age.equals(that.age) : that.age == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        return result;
+    }
+}
