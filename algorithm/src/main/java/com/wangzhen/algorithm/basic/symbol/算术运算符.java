bd0c0120e66ebff0e9b966aafd72aa01cc781868
wangzhen <1228857713@qq.com>
2020-10-24 19:33:26 +0800

    add file

---
+++ algorithm/src/main/java/com/wangzhen/algorithm/basic/symbol/算术运算符.java
@@ -0,0 +1,17 @@
+package com.wangzhen.algorithm.basic.symbol;
+
+import org.junit.Test;
+
+/**
+ * Description:
+ * Datetime:    2020/10/24   6:01 下午
+ * Author:   王震
+ */
+public class 算术运算符 {
+
+    @Test
+    public void test1(){
+        System.out.println(54235%15);
+        System.out.println(54235&15);
+    }
+}
